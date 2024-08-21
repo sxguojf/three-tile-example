b7ae4a72f92656ae3f25c2bafba9e974a8ca905b
hz_gjf@163.com <gjf123456>
2024-08-21 12:05:24 +0800

    -

---
+++ src/step2.12/MyViewer.ts
@@ -0,0 +1,175 @@
+/**
+ *@description: threejs 3D scene initalize
+ *@author: Guojf
+ *@date: 2023-04-05
+ */
+
+import {
+	AmbientLight,
+	BaseEvent,
+	Clock,
+	Color,
+	DirectionalLight,
+	EventDispatcher,
+	FogExp2,
+	MathUtils,
+	PerspectiveCamera,
+	Scene,
+	Vector3,
+	WebGLRenderer,
+} from "three";
+
+import { MapControls } from "three/examples/jsm/controls/MapControls";
+
+interface ViewerEventMap extends BaseEvent {
+	update: BaseEvent | { delta: number };
+}
+
+/**
+ * threejs scene viewer initialize class
+ */
+export class MyViewer extends EventDispatcher<ViewerEventMap> {
+	public readonly scene: Scene;
+	public readonly renderer: WebGLRenderer;
+	public readonly camera: PerspectiveCamera;
+	public readonly controls: MapControls;
+	public readonly ambLight: AmbientLight;
+	public readonly dirLight: DirectionalLight;
+	public readonly container: HTMLElement;
+	private readonly _clock: Clock = new Clock();
+
+	private _fogFactor = 1.0;
+	public get fogFactor() {
+		return this._fogFactor;
+	}
+	public set fogFactor(value) {
+		this._fogFactor = value;
+		this.controls.dispatchEvent({ type: "change" });
+	}
+
+	public get width() {
+		return this.container.clientWidth;
+	}
+
+	public get height() {
+		return this.container.clientHeight;
+	}
+
+	constructor(
+		container: HTMLElement | string,
+		options = { centerPostion: new Vector3(0, 0, -3000), cameraPosition: new Vector3(0, 30000, 0) },
+	) {
+		super();
+		const el = typeof container === "string" ? document.querySelector(container) : container;
+		if (el instanceof HTMLElement) {
+			this.container = el;
+			this.renderer = this._createRenderer();
+			this.scene = this._createScene();
+			this.camera = this._createCamera(options.cameraPosition);
+			this.controls = this._createControls(options.centerPostion);
+			this.ambLight = this._createAmbLight();
+			this.scene.add(this.ambLight);
+			this.dirLight = this._createDirLight(options.centerPostion);
+			this.scene.add(this.dirLight);
+			this.container.appendChild(this.renderer.domElement);
+			window.addEventListener("resize", this.resize.bind(this));
+			this.resize();
+			this.renderer.setAnimationLoop(this.animate.bind(this));
+		} else {
+			throw `${container} not found!}`;
+		}
+	}
+
+	private _createScene() {
+		const scene = new Scene();
+		const backColor = 0xdbf0ff;
+		scene.background = new Color(backColor);
+		scene.fog = new FogExp2(backColor, 0);
+		return scene;
+	}
+
+	private _createRenderer() {
+		const renderer = new WebGLRenderer({
+			antialias: false,
+			alpha: true,
+			// logarithmicDepthBuffer: true,
+			precision: "highp",
+		});
+		renderer.debug.checkShaderErrors = true;
+		// renderer.toneMapping = 3;
+		// renderer.toneMappingExposure = 1;
+
+		renderer.sortObjects = true;
+		renderer.setPixelRatio(window.devicePixelRatio);
+
+		return renderer;
+	}
+
+	private _createCamera(pos: Vector3) {
+		const camera = new PerspectiveCamera(70, 1, 0.1, 50000);
+		camera.position.copy(pos);
+		return camera;
+	}
+
+	private _createControls(centerPos: Vector3) {
+		const controls = new MapControls(this.camera, this.container);
+		controls.target.copy(centerPos);
+		controls.screenSpacePanning = false;
+		controls.minDistance = 0.1;
+		controls.maxDistance = 30000;
+		// controls.maxPolarAngle = 1.2;
+		controls.enableDamping = true;
+		controls.keyPanSpeed = 5;
+
+		controls.listenToKeyEvents(window);
+		controls.addEventListener("change", () => {
+			// camera polar
+			const polar = Math.max(this.controls.getPolarAngle(), 0.1);
+			// dist of camera to controls
+			const dist = Math.max(this.controls.getDistance(), 0.1);
+
+			// set zoom speed on dist
+			controls.zoomSpeed = Math.max(Math.log(dist), 1.8);
+
+			// set far and near on dist/polar
+			this.camera.far = MathUtils.clamp((dist / polar) * 800, 100, 500000);
+			this.camera.near = this.camera.far / 1000;
+			this.camera.updateProjectionMatrix();
+
+			// set fog density on dist/polar
+			if (this.scene.fog instanceof FogExp2) {
+				this.scene.fog.density = (polar / dist) * this.fogFactor * 0.1;
+			}
+		});
+		return controls;
+	}
+
+	private _createAmbLight() {
+		const ambLight = new AmbientLight(0xffffff, 1);
+		return ambLight;
+	}
+
+	private _createDirLight(center: Vector3) {
+		const dirLight = new DirectionalLight(0xffffff, 1);
+		// dirLight.position.set(-1e3, 1e4, -2e3);
+		dirLight.position.set(0, 2e3, 1e3);
+		dirLight.target.position.copy(center);
+		return dirLight;
+	}
+
+	public resize() {
+		const width = this.width;
+		const height = this.height;
+		this.renderer.setPixelRatio(window.devicePixelRatio);
+		this.renderer.setSize(width, height);
+		this.camera.aspect = width / height;
+		this.camera.updateProjectionMatrix();
+		return this;
+	}
+
+	private animate() {
+		this.dispatchEvent({ type: "update", delta: this._clock.getDelta() });
+		this.controls.update();
+		this.renderer.render(this.scene, this.camera);
+	}
+}
--- src/step2.12/main.ts
+++ src/step2.12/main.ts
@@ -1,93 +1,98 @@
-import { Loader3DTiles, Runtime, Shading } from "three-loader-3dtiles";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-
-import {
-	Clock,
-	GridHelper,
-	LinearSRGBColorSpace,
-	MeshLambertMaterial,
-	PerspectiveCamera,
-	Scene,
-	Vector2,
-	WebGLRenderer,
-} from "three";
-
+import { Vector3 } from "three";
+import * as ms from "../mapSource";
+import * as util from "../util";
+import { MyViewer } from "./MyViewer";
 import "./style.css";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-const scene = new Scene();
+import { Loader3DTiles, PointCloudColoring, Runtime } from "three-loader-3dtiles";
 
-const gridHelper = new GridHelper(1000, 10);
-scene.add(gridHelper);
+// 创建地图
+//-----------------------------------------------------------------------------------------------------
+const map = util.createMap(ms.mapBoxImgSource, ms.mapBoxDemSource);
+map.scale.setScalar(1000);
+// 地图中心
+const centerPostion = map.localToWorld(map.geo2pos(new Vector3(144.94345786971536, -37.812765742471754, 0)));
+const cameraPosition = map.localToWorld(map.geo2pos(new Vector3(144.94345786971536, -37.82, 2)));
+// 使用自定义类初始化三维场景
+const viewer = new MyViewer("#map", { centerPostion, cameraPosition });
+// 将地图加入三维场景
+viewer.scene.add(map);
 
-const camera = new PerspectiveCamera();
-camera.position.set(0, 0, 100);
-
-const renderer = new WebGLRenderer();
-renderer.outputColorSpace = LinearSRGBColorSpace;
-
-const clock = new Clock();
-const controls = new OrbitControls(camera, renderer.domElement);
-
-const canvasParent = document.querySelector("#map")!;
-
-canvasParent.appendChild(renderer.domElement);
-
+//---------------------------------------------------------------
 let tilesRuntime: Runtime | undefined = undefined;
 
-const viewportSize = new Vector2();
+viewer.addEventListener("update", (evt: any) => {
+	if (tilesRuntime) {
+		tilesRuntime.update(evt.delta, viewer.camera);
+	}
+});
+
+//---------------------------------------------------------------
 
 async function loadTileset() {
 	const result = await Loader3DTiles.load({
 		// url: "https://int.nyt.com/data/3dscenes/ONA360/TILESET/0731_FREEMAN_ALLEY_10M_A_36x8K__10K-PN_50P_DB/tileset_tileset.json",
-		url: "../3dtiles/qx-dyt/tileset.json",
+		// url: "../3dtiles/qx-dyt/tileset.json",
 		// url: "http://gisbg.scyttx.com.cn/gisdata/9171/tileset.json",
+		url: "https://assets.ion.cesium.com/43978/tileset.json",
 		viewport: getViewport(),
 		options: {
+			cesiumIONToken:
+				"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3NjEwMjA4Ni00YmVkLTQyMjgtYjRmZS1lY2M3ZWFiMmFmNTYiLCJpZCI6MjYxMzMsImlhdCI6MTY3NTM2ODY4NX0.chGkGL6DkDNv5wYJQDMzWIvi9iDoVa27dgng_5ARDmo",
 			dracoDecoderPath: "https://unpkg.com/browse/three@0.165.0/examples/jsm/libs/draco",
 			basisTranscoderPath: "https://unpkg.com/browse/three@0.165.0/examples/jsm/libs/basis",
 			resetTransform: true,
-			debug: true,
-			// gltfLoader: new GLTFLoader(),
-			shading: Shading.ShadedTexture,
-			wireframe: true,
+			// debug: true,
+			// maximumScreenSpaceError: 10,
+			memoryAdjustedScreenSpaceError: true,
+			pointCloudColoring: PointCloudColoring.RGB,
 		},
 	});
 	const { model, runtime } = result;
-	scene.add(model);
-	scene.add(runtime.getTileBoxes());
+
+	// runtime.orientToGeocoord({
+	// 	long: runtime.getTileset().cartographicCenter[0],
+	// 	lat: runtime.getTileset().cartographicCenter[1],
+	// 	height: runtime.getTileset().cartographicCenter[2],
+	// });
+
+	// const coords = runtime.getWebMercatorCoord({
+	// 	long: runtime.getTileset().cartographicCenter[0],
+	// 	lat: runtime.getTileset().cartographicCenter[1],
+	// 	height: 0,
+	// });
+
+	// map.position.set(coords.x, -200, coords.y);
+
+	model.position.copy(centerPostion);
+	model.position.setY(200);
+	// model.scale.setScalar(0.001);
+	viewer.scene.add(model);
+	viewer.scene.add(runtime.getTileBoxes());
 
 	tilesRuntime = runtime;
-
 	model.rotation.set(-Math.PI / 2, 0, 0);
-}
 
-function render() {
-	const dt = clock.getDelta();
-	controls.update();
-	if (tilesRuntime) {
-		tilesRuntime.update(dt, camera);
-	}
-	renderer.render(scene, camera);
-	window.requestAnimationFrame(render);
-}
+	const long = runtime.getTileset().cartographicCenter[0];
+	const lat = runtime.getTileset().cartographicCenter[1];
+	const height = runtime.getTileset().cartographicCenter[2];
 
-function onWindowResize() {
-	renderer.setSize(canvasParent.clientWidth, canvasParent.clientHeight);
-	tilesRuntime?.setViewport(getViewport());
-	camera.aspect = canvasParent.clientWidth / canvasParent.clientHeight;
-	camera.updateProjectionMatrix();
+	console.log(long, lat, height);
 }
-window.addEventListener("resize", onWindowResize);
 
 function getViewport() {
 	return {
-		width: canvasParent.clientWidth,
-		height: canvasParent.clientHeight,
+		width: viewer.width,
+		height: viewer.height,
 		devicePixelRatio: window.devicePixelRatio,
 	};
 }
 
+//---------------------------------------------------------------
 loadTileset();
-onWindowResize();
-render();
+util.addMapBackground(map);
+util.showLoadstate(map);
+util.showLocation(viewer as any, map);
+util.showAttribution(map);
+util.showCameraInfo(viewer as any);
+util.addSky(viewer as any);
