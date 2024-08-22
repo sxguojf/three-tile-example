6b6ebdb55325145028e6580ba7c7f8d7c0d2f795
hz_gjf@163.com <gjf123456>
2024-08-22 17:52:19 +0800

    -

--- src/step2.12/main.ts
+++ src/step2.12/main.ts
@@ -11,8 +11,8 @@ import { Loader3DTiles, PointCloudColoring, Runtime } from "three-loader-3dtiles
 const map = util.createMap(ms.mapBoxImgSource, ms.mapBoxDemSource);
 map.scale.setScalar(1000);
 // 地图中心
-const centerPostion = map.localToWorld(map.geo2pos(new Vector3(144.94345786971536, -37.812765742471754, 0.02)));
-const cameraPosition = map.localToWorld(map.geo2pos(new Vector3(144.94345786971536, -37.82, 2)));
+const centerPostion = map.localToWorld(map.geo2pos(new Vector3(108, 34, 0)));
+const cameraPosition = map.localToWorld(map.geo2pos(new Vector3(108, 30, 1000)));
 // 使用自定义类初始化三维场景
 const viewer = new MyViewer("#map", { centerPostion, cameraPosition });
 // 将地图加入三维场景
@@ -31,52 +31,49 @@ viewer.addEventListener("update", (evt: any) => {
 
 async function loadTileset() {
 	const result = await Loader3DTiles.load({
-		// url: "https://int.nyt.com/data/3dscenes/ONA360/TILESET/0731_FREEMAN_ALLEY_10M_A_36x8K__10K-PN_50P_DB/tileset_tileset.json",
-		// url: "../3dtiles/qx-dyt/tileset.json",
-		// url: "http://gisbg.scyttx.com.cn/gisdata/9171/tileset.json",
-		url: "https://assets.ion.cesium.com/43978/tileset.json",
-		viewport: getViewport(),
+		url: "https://assets.ion.cesium.com/69380/tileset.json",
+		viewport: {
+			width: viewer.width,
+			height: viewer.height,
+			devicePixelRatio: window.devicePixelRatio,
+		},
 		options: {
 			cesiumIONToken:
 				"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3NjEwMjA4Ni00YmVkLTQyMjgtYjRmZS1lY2M3ZWFiMmFmNTYiLCJpZCI6MjYxMzMsImlhdCI6MTY3NTM2ODY4NX0.chGkGL6DkDNv5wYJQDMzWIvi9iDoVa27dgng_5ARDmo",
-			dracoDecoderPath: "https://unpkg.com/browse/three@0.160.0/examples/jsm/libs/draco",
-			basisTranscoderPath: "https://unpkg.com/browse/three@0.160.0/examples/jsm/libs/basis",
+			dracoDecoderPath: "../lib/draco",
 			resetTransform: true,
-			debug: true,
+			// debug: true,
 			// maximumScreenSpaceError: 10,
 			memoryAdjustedScreenSpaceError: true,
 			pointCloudColoring: PointCloudColoring.RGB,
 		},
 	});
 	const { model, runtime } = result;
-
-	model.position.copy(centerPostion);
-	model.position.setY(150);
-	// model.scale.setScalar(0.001);
-	viewer.scene.add(model);
-	viewer.scene.add(runtime.getTileBoxes());
-
 	tilesRuntime = runtime;
-	model.rotation.set(-Math.PI / 2, 0, 0);
 
 	const tileset = runtime.getTileset();
 	const long = tileset.cartographicCenter[0];
 	const lat = tileset.cartographicCenter[1];
 	const height = tileset.cartographicCenter[2];
 
+	const center = map.localToWorld(map.geo2pos(new Vector3(long, lat, 0)));
+	model.position.copy(center);
+	model.position.setY(200);
+	model.scale.setScalar(1.3);
+	model.rotation.set(-Math.PI / 2, 0, 0);
+	viewer.scene.add(model);
+	viewer.scene.add(runtime.getTileBoxes());
+
+	const cameraPosition = new Vector3(center.x - 400, 250, center.z - 200);
+	viewer.camera.position.copy(cameraPosition);
+	viewer.controls.target.copy(center);
+
 	console.log(long, lat, height);
 }
 
-function getViewport() {
-	return {
-		width: viewer.width,
-		height: viewer.height,
-		devicePixelRatio: window.devicePixelRatio,
-	};
-}
+loadTileset();
 
 //---------------------------------------------------------------
-loadTileset();
 util.addMapBackground(map);
 util.showLoadstate(map);
 util.showLocation(viewer as any, map);
